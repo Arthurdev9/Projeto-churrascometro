cf6e85e359cbfc46fa1f138e71d6eb181e704e4a
Arthur Vinicius <arthurvinicius081@hotmail.com>
2025-07-19 10:26:52 -0300

    Implementação completa da lógica e estilização do componente Resultado do churrasco.

--- src/components/ResultadoChurrasco.tsx
+++ src/components/ResultadoChurrasco.tsx
@@ -1,5 +1,45 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import { type Alimentos, nomeAlimentos, quantidadePessoas } from '../types'
+
+import styles from './ResultadoChurrasco.module.css'
+
+type ResultadosChurrasco = {
+  pessoas: number
+  alimentosSelecionados: Alimentos[]
+}
+
 const ResultadoChurrasco = () => {
-  return <div>ResultadoChurrasco</div>
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const state = location.state as ResultadosChurrasco
+
+  const totalPorAlimento = state.alimentosSelecionados.reduce(
+    (acc, alimento) => {
+      acc[alimento] = (quantidadePessoas[alimento] * state.pessoas) / 1000
+      return acc
+    },
+    {} as Record<Alimentos, number>,
+  )
+
+  const reiniciar = () => {
+    navigate('/')
+  }
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Resultado para {state.pessoas} pessoas: </h2>
+      {state.alimentosSelecionados.map((alimento) => (
+        <p key={alimento} className={styles.resultText}>
+          {nomeAlimentos[alimento]}: {totalPorAlimento[alimento]} kg
+        </p>
+      ))}
+      <button onClick={reiniciar} className={styles.resetButton}>
+        Reiniciar
+      </button>
+    </div>
+  )
 }
 
 export default ResultadoChurrasco
